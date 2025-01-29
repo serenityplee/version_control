be82d6f1952897a4aa74f1f4daaa215c9899f625
serenipl <serenipl@uci.edu>
2025-01-29 21:34:17 +0000

    new update

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-with open("../version.md", "w") as f:
+with open("version.md", "w") as f:
     f.write(f"Time: {time}\n")
 
 print("updated")
