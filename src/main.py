ce59d806cf31d67ed7f4a5529f2756640b580691
serenipl <serenipl@uci.edu>
2025-01-29 21:10:16 +0000

    Added main.py to update version.md

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-date = ["import datetime\n","print datetime.datetime.now()\n"]
+datetime = ["import datetime\n","print datetime.datetime.now()\n"]
 
 with open("version.md", "w") as file:
-    file.writelines(date)
+    file.writelines(datetime)
